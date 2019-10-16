4a1ac90dbb55c754a8b5c07503fee4e25094c28f
samarthhegdekalgar <samarthhegdekalgar@gmail.com>
2019-10-17 00:19:55 +0530

    updated UI

--- library/libraryApp/admin.py
+++ library/libraryApp/admin.py
@@ -6,28 +6,65 @@ from .models import Libraries, Librarian, Book, Author, Member, Record
 class FormRecordAdmin(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(FormRecordAdmin, self).__init__(*args, **kwargs)
-        if not self.instance.borrowed_ID:
+        if not self.instance.borrowed_ID or self.instance.is_return:
             self.fields['is_return'].disabled = True
             self.fields['returned_date'].disabled = True
+        self.fields['time'].disabled = True
 
     def clean(self):
-        member_id = self.cleaned_data.get('borrowed_member')
-        borrow_count = Record.objects.filter(borrowed_member__member_ID=member_id.member_ID).filter(is_return=False).count()
-        if borrow_count > 4:
+        member_fk = self.cleaned_data.get('borrowed_member')
+        is_return = self.cleaned_data.get('is_return')
+        book_fk = self.cleaned_data.get('borrowed_book')
+        borrow_count = Record.objects.filter(borrowed_member__member_ID=member_fk.member_ID).\
+            filter(is_return=False).count()
+
+        if borrow_count > 4 and not is_return:
             raise forms.ValidationError(f'Book borrow count is exceeded!', code='invalid')
+        if book_fk.stock <= 0 and not is_return:
+            raise forms.ValidationError(f'{book_fk.book_name} not available', code='not available')
+
         return self.cleaned_data
+
+    """
+    if borrowed_book and borrowed_member and not is_return:
+        raise forms.ValidationError(f'{borrowed_book__book_name} is already borrowed 
+        by {borrowed_member__member_name}',code='book issued')
+        
+    """
 
     def save(self, commit=True):
         return super(FormRecordAdmin, self).save(commit=commit)
 
 
 class RecordAdmin(admin.ModelAdmin):
+    list_display = ('borrowed_ID', 'borrowed_member', 'borrowed_book', 'issued_librarian', 'issue_date', 'return_date',
+                    'is_return', 'returned_date')
     form = FormRecordAdmin
 
 
-admin.site.register(Libraries)
-admin.site.register(Librarian)
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Member)
+class LibrariesAdmin(admin.ModelAdmin):
+    list_display = ('library_id', 'library_address','library_contact_number')
+
+
+class LibrarianAdmin(admin.ModelAdmin):
+    list_display = ('librarian_ID', 'librarian_name', 'librarian_contact_no', 'belong_to')
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('author_name', 'about_author',)
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('isbn', 'book_name', 'category', 'number_of_copy', 'stock', 'is_available', 'price')
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('member_ID', 'member_name', 'member_contact_number', 'member_email', 'member_address')
+
+
+admin.site.register(Libraries, LibrariesAdmin)
+admin.site.register(Librarian, LibrarianAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Member, MemberAdmin)
 admin.site.register(Record, RecordAdmin)
--- library/libraryApp/models.py
+++ library/libraryApp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
+from django.db.models import signals
+from django.dispatch import receiver
 
 
 class Libraries(models.Model):
-    library_id = models.AutoField(primary_key=True)
+    library_id = models.AutoField(primary_key=True, verbose_name='ID')
     library_address = models.TextField(verbose_name='Address', help_text='Enter library address')
     library_contact_number = models.IntegerField(verbose_name='Phone number', help_text='Enter library contact number')
 
@@ -12,7 +14,7 @@ class Libraries(models.Model):
 
 
 class Librarian(models.Model):
-    librarian_ID = models.AutoField(primary_key=True)
+    librarian_ID = models.AutoField(primary_key=True, verbose_name='Employee ID')
     librarian_name = models.CharField(verbose_name='Name', max_length=100, help_text='Enter Librarian Name')
     librarian_contact_no = models.IntegerField(verbose_name='Phone number', help_text='Enter Librarian contact number')
     belong_to = models.ForeignKey(Libraries,verbose_name='Library ID', help_text='Select library ID',
@@ -38,7 +40,7 @@ class Book(models.Model):
     number_of_copy = models.IntegerField(verbose_name='Book count', default=1, help_text='Enter the number of copies '
                                                                                          'available')
     stock = models.IntegerField(verbose_name='Stock', help_text='Enter the Book available', default=1)
-    availability = models.BooleanField(default=True)
+    availability = models.BooleanField(default=True, verbose_name='Available')
     description = models.TextField(verbose_name='Description', help_text='Write something about book', null=True)
     price = models.IntegerField(verbose_name='Price', help_text='Enter the price of book', default=0)
     book_author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -46,9 +48,17 @@ class Book(models.Model):
     def __str__(self):
         return self.book_name
 
+    def is_available(self):
+        if self.stock > 0:
+            return True
+        else:
+            return False
+    is_available.boolean = True
+    is_available.short_description = 'Available'
+
 
 class Member(models.Model):
-    member_ID = models.AutoField(primary_key=True)
+    member_ID = models.AutoField(primary_key=True, verbose_name='ID')
     member_name = models.CharField(verbose_name='Name', help_text='Enter member name', max_length=100)
     member_contact_number = models.IntegerField(verbose_name='Phone number', help_text='Enter member phone number')
     member_email = models.EmailField(verbose_name='Email', help_text='Enter member Email')
@@ -59,25 +69,51 @@ class Member(models.Model):
 
 
 class Record(models.Model):
-    borrowed_ID = models.AutoField(primary_key=True)
+    borrowed_ID = models.AutoField(primary_key=True, verbose_name='Order ID')
     borrowed_member = models.ForeignKey(Member, on_delete=models.CASCADE, verbose_name='Member name',
                                         help_text='Select Member name')
     borrowed_book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name='Book name',
                                       help_text='Select Book name')
     issued_librarian = models.ForeignKey(Librarian, on_delete=models.CASCADE, verbose_name='Issued Librarian',
                                          help_text='Select librarian')
-    book_quantity = models.IntegerField(verbose_name='Number of Book', help_text='Enter book quantity', default=1)
     issue_date = models.DateField(verbose_name='Issued Date', auto_now_add=True)
     return_date = models.DateField(default=date.today() + timedelta(days=7), verbose_name='Return date',
                                    help_text='Enter return date')
     is_return = models.BooleanField(default=False, help_text='Select if book is returning', verbose_name='Return')
-    returned_date = models.DateField(verbose_name='Returned date', help_text='Enter if book returned', null=True)
+    returned_date = models.DateField(verbose_name='Returned date', help_text='Enter if book returned',
+                                     default=date.today() + timedelta(days=7))
+    time = models.TextField(default=datetime.now())
 
     class Meta:
-        unique_together = ['borrowed_member', 'borrowed_book', 'is_return']
+        unique_together = ['borrowed_member', 'borrowed_book', 'is_return', 'time']
 
     def is_due(self):
         pass
 
     def __str__(self):
         return str(self.borrowed_ID)
+
+
+@receiver(signals.post_save, sender=Record)
+def stock_decrement(sender, instance, created, **kwargs):
+    stock = instance.borrowed_book.stock
+    if created:
+        if not instance.is_return:
+            obj = instance.borrowed_book
+            stock -= 1
+            obj.stock = stock
+            if stock <= 0:
+                obj.availability = False
+            obj.save()
+
+
+@receiver(signals.pre_save, sender=Record)
+def stock_increment(sender, instance, **kwargs):
+    stock = instance.borrowed_book.stock
+    if instance.is_return:
+        stock += 1
+        obj = instance.borrowed_book
+        obj.stock = stock
+        if obj.stock > 0:
+            obj.availability = True
+        obj.save()
